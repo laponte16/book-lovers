4216088c63814303382e0f9d8bab6e64a02c14e6
mcastelli88 <mcastelliromero@gmail.com>
2022-08-25 12:42:34 -0400

    mobile

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 var path = require('path');
+var useragent = require('express-useragent');
 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.get("/", (req, res) => {
   res.render("./index");
+  res.send(req.useragent.isMobile);
 });
 
 app.get("/home", (req, res) => {
