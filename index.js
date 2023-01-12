374b9cf068543d5372d99f5ac10d18ddbf585b63
diegoxi <diegomil541@gmail.com>
2023-01-12 17:41:15 -0400

    adios show

--- index.js
+++ index.js
@@ -331,33 +331,7 @@ app.get("/post/:post_id",(req, res) => {
 });
 
 /*Query para mostrar los generos segun el id nota no fiatra*/
-app.get("/showGen/:id_genres",(req, res) => {
-  pool.connect();
 
-  const text = 'SELECT * FROM posts WHERE id_genre = $1';
-  const values = [req.params.id_genres];
-
-  pool.query(text, values, (err, result) => {
-
-    posts = result.rows;
-
-    const text1 = 'SELECT * FROM genres WHERE id_genres = $1';
-    const values1 = [req.params.id_genres];
-
-    pool.query(text1, values1, (err, result1) => {
-
-    genres = result1.rows;
-
-    var obj = {};
-    obj.posts = posts;
-    obj.genres = genres;
-    obj.session = req.session;
- 
-    res.render("./showgen.ejs" , {result: obj} );
-      
-    });
-  });
-});
 /*Query para llevarte a la pagina de creacion de posts*/
 app.get("/addPost",(req, res) => {
   pool.connect();
