f92a34ebb0f8088ba0857d1ffa47ed5a2f670881
mcastelli88 <mcastelliromero@gmail.com>
2023-01-13 17:08:15 -0400

    commitsito

---
+++ public/js/home.js
@@ -0,0 +1,22 @@
+var x;
+	x=$(document);
+	x.ready(start);//inico de la logica del documento
+
+    var arrowLeftHot = $(".arrowLeftHot");
+    var arrowRightHot = $(".arrowRightHot");
+
+    var panelHot = $(".panelHot");
+
+	function start() {
+		arrowLeftHot.on("click",clickArrowLeftHot);
+        arrowRightHot.on("click",clickArrowRightHot);
+	}
+	function clickArrowLeftHot() {
+		
+		
+	}
+
+    function clickArrowRightHot() {
+		
+		
+	}
