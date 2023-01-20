ffb4eb179c521ec136567b02df52c00b5f09101d
diegoxi <diegomil541@gmail.com>
2023-01-20 18:15:22 -0400

    real dark

---
+++ views/boton.ejs.php
@@ -0,0 +1,6 @@
+<div class="modo" id="modo" style="display: block;">
+        <i class="fas fa-toggle-on"></i>
+    </div>
+     <div class="luz" id="luz" style="display:">
+        <i class="fas fa-toggle-on"></i>
+    </div>
